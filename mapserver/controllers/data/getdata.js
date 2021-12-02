ed91b93a59be934ba6e0197e467607ac2071415d
Đạt Nguyễn Thành <18520580@ms.uit.edu.vn>
2021-12-02 21:53:33 +0700

    update helper function

--- mapserver/controllers/data/getdata.js
+++ mapserver/controllers/data/getdata.js
@@ -1,24 +1,23 @@
 var {
     geoTemplateData,
     geoTemplate,
-    getPointFromArray,
-    getRect
+    getBox
 } = require('../../helper/createpolygon');
 
 const getnen = (req, res, next) => {
 
-    const startPoint = [106.70675051181462, 10.768089872127144];
-    const rect = getRect(getPointFromArray(startPoint), 66, 33.5, 26.6)
-    
-    rect[0].push(5.5)
-    rect[1].push(5)
-    rect[2].push(5)
-    rect[3].push(5)
-    rect[4].push(5.5)
+    const box = getBox({
+        startPoint: [106.70675051181462, 10.768089872127144],
+        bearing: 66,
+        width: 33,
+        length: 26,
+        heights: [5.5, 5, 5, 5, 5.5]
+    })
 
     const result = geoTemplate()
-    result["features"].push(geoTemplateData("Nền nhà", [rect]))
+    result["features"].push(geoTemplateData("Nền nhà", [box]))
     res.send(result)
+
 }
 
 
--- mapserver/helper/createpolygon.js
+++ mapserver/helper/createpolygon.js
@@ -58,34 +58,10 @@ const getBox = (data) => {
 }
 
 
-
-
-// /* GET home page. */
-// router.get('/', (req, res, next) => {
-//     res.render("map")
-// });
-
-// router.get('/nen', (req, res, next) => {
-
-//     const box = getBox({
-//         startPoint: [106.70675051181462, 10.768089872127144],
-//         bearing: 66,
-//         width: 33,
-//         length: 26,
-//         heights: [5.5, 5, 5, 5, 5.5]
-//     })
-
-//     const result = geoTemplate()
-//     result["features"].push(geoTemplateData("Nền nhà", [box]))
-//     res.send(result)
-// });
-
-
-
-// module.exports = router;
 module.exports = {
     geoTemplateData,
     geoTemplate,
     getPointFromArray,
-    getRect
+    getRect,
+    getBox,
 }
