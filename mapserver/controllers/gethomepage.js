ae1570c93e655ec77b25c40cae552652c49d2496
Long Nguyễn Thanh <18521045@ms.uit.edu.vn>
2021-12-05 00:12:58 +0700

    Merch code ui home page

--- mapserver/controllers/gethomepage.js
+++ mapserver/controllers/gethomepage.js
@@ -2,6 +2,11 @@ const gethomepage = function (req, res, next) {
     res.render('HomePage');
 }
 
+const getaboutus = function (req, res, next) {
+    res.render('AboutUs');
+}
+
 module.exports = {
-    gethomepage
+    gethomepage,
+    getaboutus
 }
--- mapserver/routes/homepage.js
+++ mapserver/routes/homepage.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {gethomepage} = require('../controllers/gethomepage.js');
+const {gethomepage,getaboutus} = require('../controllers/gethomepage.js');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/',gethomepage);
+router.get('/about',getaboutus);
 
 module.exports = router;
