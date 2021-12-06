62fd2a6a203c63f6f85271ffa65fcd03998a8ba4
DucTran2310 <18520021@gm.uit.edu.vn>
2021-12-06 19:03:44 +0700

    Add admin fence

--- mapserver/routes/admin/admin.js
+++ mapserver/routes/admin/admin.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const {getadminpage,postadminpage,getadminpagecirculation} = require('../../controllers/admin/adminpage');
-const {auth} = require('../../middlewares/auth');
+const express = require("express");
+const {
+    getadminpage,
+    postadminpage,
+    getadminpagecirculation,
+} = require("../../controllers/admin/adminpage");
+const { auth } = require("../../middlewares/auth");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/',auth,getadminpage);
-router.post('/',auth,postadminpage);
-router.get('/admin_circulation',auth,getadminpagecirculation);
+router.get("/", auth, getadminpage);
+router.post("/", auth, postadminpage);
+router.get("/admin_circulation", auth, getadminpagecirculation);
 
 module.exports = router;
